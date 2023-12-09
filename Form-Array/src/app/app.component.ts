a08b7bc8098d9a2c45f71898025b9a1007d20c39
Arshia.Hsh <arshia.hsh72@gmail.com>
2023-12-10 00:10:11 +0330

    second commit

--- Form-Array/src/app/app.component.ts
+++ Form-Array/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormGroup,FormBuilder,FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,24 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Form-Array';
+  myForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      items: this.fb.array([])
+    });
+  }
+
+  get items() {
+    return this.myForm.get('items') as FormArray;
+  }
+
+  addItem() {
+    this.items.push(this.fb.control(''));
+  }
+
+  removeItem(index: number) {
+    this.items.removeAt(index);
+  }
 }
+
